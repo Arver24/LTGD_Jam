042ba64ea84cc27e31c28d93391b5d66dbe328e4
Arver24 <arver2405@gmail.com>
2022-08-18 03:34:30 +0530

    Added Game Manager and player lookaround

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D rb;
     [SerializeField] float speed;
+    float currAngle;
 
     void Start()
     {
@@ -18,22 +19,27 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 xMovement = new Vector2(1, 0);
         Vector2 yMovement = new Vector2(0, 1);
+        currAngle = rb.rotation;
 
         if (Input.GetKey(KeyCode.W))
         {
             rb.position += yMovement * speed * Time.fixedDeltaTime;
+            rb.MoveRotation(0);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             rb.position += -yMovement * speed * Time.fixedDeltaTime;
+            rb.MoveRotation(180);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             rb.position += -xMovement * speed * Time.fixedDeltaTime;
+            rb.MoveRotation(90);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.position += xMovement * speed * Time.fixedDeltaTime;
+            rb.MoveRotation(-90);
         }
         
 
